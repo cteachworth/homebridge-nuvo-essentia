234f9784b2eb71028d8c68ba8da45f4dbbb7cc67
Chris Teachworth <334401+cteachworth@users.noreply.github.com>
2021-09-05 16:46:00 -0400

    bah

--- src/Essentia.ts
+++ src/Essentia.ts
@@ -195,7 +195,7 @@ class Essentia {
     }
 
     async setBass(zoneId :number, bass :number): Promise<boolean>{
-      let level = bass >= 0 ? '+' + bass : bass;
+      let level = bass >= 0 ? '+' + bass : '';
       level += String(bass).padStart(2, '0');
       this.log.debug(`Setting bass of zone ${zoneId} to ${level}`);
       const cmd = `*Z0${zoneId}BASS${level}\r`;
@@ -206,7 +206,7 @@ class Essentia {
     }
 
     async setTreble(zoneId :number, treble :number): Promise<boolean>{
-      let level = treble >= 0 ? '+' + treble : treble;
+      let level = treble >= 0 ? '+' : '';
       level += String(treble).padStart(2, '0');
       this.log.debug(`Setting treble of zone ${zoneId} to ${level}`);
       const cmd = `*Z0${zoneId}TREB${level}\r`;
